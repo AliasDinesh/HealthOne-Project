cd83e014f21c5163541a2a4e3b44d8ff307e2936
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-01-23 18:43:53 +0100

    Made two functions

--- Modules/Categories.php
+++ Modules/Categories.php
@@ -1,11 +1,19 @@
 <?php
-// TODO Zorg dat de methodes goed ingevuld worden met de juiste queries.
+
 function getCategories()
 {
-    
+    global $pdo;
+    $categories = $pdo->query('SELECT * FROM category')->fetchAll(PDO::FETCH_CLASS, 'Category');
+    return $categories;
 }
 
-function getCategoryName(int $id)
+function getCategoryName(int $id):string
 {
-    
+    global $pdo;
+    $sth = $pdo->prepare('SELECT * FROM category WHERE id = ?');
+    $sth->bindParam(1, $id, PDO::PARAM_INT);
+    $sth->setFetchMode(PDO::FETCH_CLASS, Category::class);
+    $sth->execute();
+    $category = $sth->fetch();
+    return $category->name;
 }
