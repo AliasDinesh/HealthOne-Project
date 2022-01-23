f181b63c539866614d876bc5c8824e0dc156f42c
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-01-23 19:28:50 +0100

    Changed a few html tags

--- Templates/home.php
+++ Templates/home.php
@@ -11,10 +11,9 @@
             include_once ('defaults/pictures.php');
             ?>
 
-            <h4>Sportcenter HealthOne</h4>
-            Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
-            Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
-            Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
+            <h1 class="home-title">Sportcenter HealthOne</h1>
+            <h4 class="home-about">Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
+                Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.</h4>
             <hr>
             <?php
             include_once ('defaults/footer.php');
