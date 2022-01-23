df590def371a1f874257149cfea503ad5193f40b
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-01-23 19:20:58 +0100

    Added 5 new default templates for admin

---
+++ Templates/admin/defaults/header.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="header-row row lead" >
+        <img class="header-img img-fluid" src='/img/healthone-logo.jpeg' alt="healthone-logo" />
+        <hr>
+    </div>
+</header>
