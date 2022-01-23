d9622fbec93bc229ff65f353357a4bbe484ab370
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-01-23 19:02:55 +0100

    Added a new template for admin named home

---
+++ Templates/admin/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php
+include_once('defaults/head.php');
+?>
+<body>
+<div class="container">
+    <?php
+    include_once ('defaults/header.php');
+    include_once ('defaults/menu.php');
+    include_once ('defaults/pictures.php');
+    ?>
+
+    <h1 class="home-title">Welkom Dinesh</h1>
+    <h4 class="home-about">:)</h4>
+    <hr>
+    <?php
+    include_once ('defaults/footer.php');
+    ?>
+</div>
+</body>
+</html>
+
