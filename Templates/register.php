a29047f3b8ef7392d4c890a2f50a3176c7fb44d6
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-02-02 01:51:23 +0100

    Made a new template named register

---
+++ Templates/register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<?php
+include_once('defaults/head.php');
+?>
+
+<body>
+
+<div class="container">
+    <?php
+    include_once('defaults/header.php');
+    include_once('defaults/menu.php');
+    include_once('defaults/pictures.php');
+    global $message;
+    ?>
+    <?php if (!empty($message)): ?>
+        <div class="alert alert-danger" role="alert">
+            <?=$message?>
+        </div>
+    <?php endif;?>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item"><a href="/register">Register</a></li>
+        </ol>
+    </nav>
+    <form method="post">
+        <div class="mb-3">
+            <label for="example2" class="form-label">First Name</label>
+            <input type="text" name="first_name" class="form-control" id="example2"
+        </div>
+        <div class="mb-3">
+            <label for="example2" class="form-label">Last Name</label>
+            <input type="text" name="last_name" class="form-control" id="example2"
+        </div>
+        <div class="mb-3">
+            <label for="example1" class="form-label">Email address</label>
+            <input type="text" class="form-control" name="email1" id="example1">
+        </div>
+        <div class="mb-3">
+            <label for="example2" class="form-label">Password</label>
+            <input type="password" name="password1" class="form-control" id="example2"
+        </div>
+        <br>
+        <button type="submit" name="register" class="btn btn-primary">Submit</button>
+    </form>
+    <hr>
+    <?php
+    include_once('defaults/footer.php');
+    ?>
+</div>
+
+</body>
+</html>
