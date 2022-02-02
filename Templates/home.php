b25f08398ce4bdc7539b72d81e1af741bda47732
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-02-02 01:43:55 +0100

    Added a if statement to check if a member was added

--- Templates/home.php
+++ Templates/home.php
@@ -9,7 +9,13 @@
             include_once ('defaults/header.php');
             include_once ('defaults/menu.php');
             include_once ('defaults/pictures.php');
+            global $member;
             ?>
+            <?php if (!empty($member)): ?>
+                <div class="alert alert-success" role="alert">
+                    Member: <?=$member?> is toegevoegd!
+                </div>
+            <?php endif;?>
 
             <h1 class="home-title">Sportcenter HealthOne</h1>
             <h4 class="home-about">Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
