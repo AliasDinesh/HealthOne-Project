f486173a9ec773d59311c705bea340fdc016e1a6
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-02-02 01:11:33 +0100

    Added two new functions named isMember and saveUser

--- Modules/Login.php
+++ Modules/Login.php
@@ -18,7 +18,7 @@ function checkLogin():string
 
         if ($user !== false) {
             $_SESSION['user'] = $user;
-            if ($_SESSION['user']->role =='admin') {
+            if ($_SESSION['user']->role == 'admin') {
                 return 'ADMIN';
             } else {
                 return 'MEMBER';
@@ -29,15 +29,51 @@ function checkLogin():string
     return 'INCOMPLETE';
 }
 
+function logOut()
+{
+    unset($_SESSION['user']);
+    session_destroy();
+}
+
 function isAdmin():bool
 {
     if (isset($_SESSION['user']) && !empty($_SESSION['user']))
     {
-        $user = $_SESSION['user'];;
+        $user = $_SESSION['user'];
         if ($user->role == "admin") {
             return true;
-        } else {}
-        return false;
+        } else {
+            return false;
+        }
+
     }
     return false;
+}
+
+function isMember():bool
+{
+    if (isset($_SESSION['user']) && !empty($_SESSION['user']))
+    {
+        $user = $_SESSION['user'];;
+        if ($user->role == "member") {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    return false;
+}
+
+function saveUser(string $email, string $password, string $first_name, string $last_name):void
+{
+    global $pdo;
+    $role = 'member';
+    $sth = $pdo->prepare('INSERT INTO `user` (email, password, first_name, last_name, role) VALUES (?, ?, ?, ?, ?)');
+    $sth->bindParam(1, $email);
+    $sth->bindParam(2, $password);
+    $sth->bindParam(3, $first_name);
+    $sth->bindParam(4, $last_name);
+    $sth->bindParam(5, $role);
+    $sth->execute();
 }
