e59a651b574da913c50f69128051593b2d2d0f1b
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-03-06 20:19:38 +0100

    Added a new if statement

--- Templates/register.php
+++ Templates/register.php
@@ -13,6 +13,11 @@ include_once('defaults/head.php');
     include_once('defaults/pictures.php');
     global $message;
     ?>
+    <?php if (!empty($member)): ?>
+        <div class="alert alert-success" role="alert">
+            Member: <?=$member?> is toegevoegd!
+        </div>
+    <?php endif;?>
     <?php if (!empty($message)): ?>
         <div class="alert alert-danger" role="alert">
             <?=$message?>
@@ -25,24 +30,24 @@ include_once('defaults/head.php');
         </ol>
     </nav>
     <form method="post">
-        <div class="mb-3">
+        <div class="mb-2">
             <label for="example2" class="form-label">First Name</label>
             <input type="text" name="first_name" class="form-control" id="example2"
         </div>
-        <div class="mb-3">
+        <div class="mb-2">
             <label for="example2" class="form-label">Last Name</label>
             <input type="text" name="last_name" class="form-control" id="example2"
         </div>
-        <div class="mb-3">
+        <div class="mb-2">
             <label for="example1" class="form-label">Email address</label>
             <input type="text" class="form-control" name="email1" id="example1">
         </div>
-        <div class="mb-3">
+        <div class="mb-2">
             <label for="example2" class="form-label">Password</label>
             <input type="password" name="password1" class="form-control" id="example2"
         </div>
         <br>
-        <button type="submit" name="register" class="btn btn-primary">Submit</button>
+        <button type="submit" name="register" class="btn btn-primary">Register</button>
     </form>
     <hr>
     <?php
