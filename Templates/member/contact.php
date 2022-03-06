f1c902f897b5c751e27ca1e9f2630c3403c2d6da
AliasDinesh <302115525@student.rocmondriaan.nl>
2022-03-06 20:10:36 +0100

    Changed the html code and added a map

--- Templates/member/contact.php
+++ Templates/member/contact.php
@@ -1,72 +1,65 @@
 <!DOCTYPE html>
 <html>
+
 <?php
 include_once('defaults/head.php');
 ?>
+
 <body>
+<style type="text/css">
+    table {
+        border-collapse: collapse;
+        border: 1px solid black;
+    }
+    td {
+        border: 1px solid black;
+        width: 150px;
+    }
+
+</style>
 <div class="container">
     <?php
-    include_once ('defaults/header.php');
-    include_once ('defaults/menu.php');
+    include_once('defaults/header.php');
+    include_once('defaults/menu.php');
+    include_once('defaults/pictures.php');
+    global $times;
     ?>
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/member/home">Home</a></li>
-            <li class="breadcrumb-item"><a href="/member/contact">Contact</a></li>
+            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item"><a href="/contact">Contact</a></li>
         </ol>
     </nav>
 
-    <h2 class="text-center">Over onze vestiging</h2>
-
-
-    <br>
-
     <div class="row">
-        <div class="col-md-10 mx-auto d-block">
-
-            <p>Zie hieronder de openingstijden en locatie van onze kantine.
-                <br>- Bij ons kunt u geen contant betalen, alleen pinnen.
-                <br>- Wij zijn niet telefonisch beschikbaar, en kunt u alleen met voorstellen komen aan de bali.
-            </p>
-
-            <?php
-            try {
-                $db = new PDO("mysql:host=localhost;dbname=kantine","root", "");
-                $query = $db->prepare ("SELECT * FROM openingstijden");
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_ASSOC);
-                echo "<table>";
-                foreach ($result as $data) {
-                    echo "<td>" . $data ["day"] . " ";
-                    echo "<td>" . $data ["time"] . "<br>";
-                    echo "</tr>";
-                }
-                echo "</table>";
-            } catch(PDOException $e) {
-                die("Error!: " . $e->getMessage());
-            }
-            ?>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h2>Locatie</h2>
+                    <h4>Zuid Hollandse weg 6a</h4> <br>
+                    <h2>Openingstijden</h2>
+                    <table>
+                        <?php foreach ($times as $time): ?>
+                            <tr>
+                                <td><?= $time->day?></td>
+                                <td ><?= $time->time?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                    <br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93541.45745641329!2d4.139378199281386!3d51.81314987519917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c44e5021cc6985%3A0xc5ffac61bc399b7d!2sHollandseweg%206%2C%203227%20CB%20Oudenhoorn!5e0!3m2!1sen!2snl!4v1646582252321!5m2!1sen!2snl" width="870" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
         </div>
     </div>
-
-    <br>
-
-    <div class="row">
-        <div class="col-sm-10 rounded mx-auto d-block">
-            <!-- <h2 class="text-center">Locatie</h2> -->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d613.4828709791777!2d4.24947072928173!3d52.04456079873292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa02397ac42a9b174!2zNTLCsDAyJzQwLjQiTiA0wrAxNScwMC4xIkU!5e0!3m2!1snl!2snl!4v1643386274592!5m2!1snl!2snl" width="1080" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            <br>
-            <h6 class="text-center">We zijn gevestigd aan Tinwerf 10, 2544 ED Den Haag</h6>
-            <br>
-        </div>
-    </div>
-
-    <br><hr><br>
-
+    <hr>
     <?php
     include_once ('defaults/footer.php');
     ?>
+
 </div>
+
 </body>
 </html>
+
